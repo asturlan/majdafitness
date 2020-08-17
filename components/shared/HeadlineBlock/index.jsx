39632a4dacef168d7189851092dd970cf3c82c9b
asturlan <adriana.sturlan@gmail.com>
2020-08-17 22:05:45 +0200

    Testing vercel

--- components/shared/HeadlineBlock/index.jsx
+++ components/shared/HeadlineBlock/index.jsx
@@ -3,12 +3,11 @@ import PropTypes from 'prop-types';
 
 import './style.scss';
 
-const HeadlineBlock = (props) => {
-    const headlineImg = require(`../../../assets/images/Homepage/introImg.jpg`);
+const HeadlineBlock = props => {
     return (
         <div className={`headline-block headline-block-${props.config.orientation !== undefined ? props.config.orientation : 'left'}`}>
             <div className="headline-img">
-                <img alt={props.config.alt} src={headlineImg}></img>
+                <img alt={props.config.alt} src={require(`../../../assets/images${props.config.imgSrc}`)}></img>
                 <div className="style-cube shadow-hard"></div>
             </div>
             <div className="headline-txt" style={{ backgroundColor: props.config.textBg }}>
