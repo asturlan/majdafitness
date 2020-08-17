72bcb8c0b2eb440266ede659320842eceb9e70e0
asturlan <adriana.sturlan@gmail.com>
2020-08-17 22:14:13 +0200

    Testing vercel

--- components/shared/HeadlineBlock/index.jsx
+++ components/shared/HeadlineBlock/index.jsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import './style.scss';
 
-const HeadlineBlock = props => {
-    return (
-        <div className={`headline-block headline-block-${props.config.orientation !== undefined ? props.config.orientation : 'left'}`}>
-            <div className="headline-img">
-                <img alt={props.config.alt} src={require(`../../../assets/images${props.config.imgSrc}`)}></img>
-                <div className="style-cube shadow-hard"></div>
+class HeadlineBlock extends Component {
+    constructor(props) {
+        super(props);
+        this.introImg = require(`../../../assets/images${this.props.config.imgSrc}`);
+    }
+
+    render() {
+        return (
+            <div className={`headline-block headline-block-${this.props.config.orientation !== undefined ? this.props.config.orientation : 'left'}`}>
+                <div className="headline-img">
+                    <img alt={this.props.config.alt} src={this.introImg}></img>
+                    <div className="style-cube shadow-hard"></div>
+                </div>
+                <div className="headline-txt" style={{ backgroundColor: this.props.config.textBg }}>
+                    {this.props.children}
+                </div>
             </div>
-            <div className="headline-txt" style={{ backgroundColor: props.config.textBg }}>
-                {props.children}
-            </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 HeadlineBlock.propTypes = {
     children: PropTypes.any,
